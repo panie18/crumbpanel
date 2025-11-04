0d1b05132b7a7943e6a83a8419c00bcbfed83886
Paul Niederle <pauli@pauli.my>
2025-11-04 20:44:23 +0100

    Remove all project files
    Deleted all source code, configuration, documentation, and setup files from both backend and frontend directories, including Docker, environment, and install scripts. This effectively removes the entire project.

--- backend/src/servers/servers.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ServersController } from './servers.controller';
-import { ServersService } from './servers.service';
-
-@Module({
-  controllers: [ServersController],
-  providers: [ServersService],
-})
-export class ServersModule {}
