c684fdf5e25594672dae4df6474758dee3ef5af6
Paul Niederle <pauli@pauli.my>
2025-11-06 21:02:19 +0100

    Update BasesMapPage.tsx

--- frontend/src/pages/BasesMapPage.tsx
+++ frontend/src/pages/BasesMapPage.tsx
@@ -358,7 +358,7 @@ export default function BasesMapPage() {
                     </Button>
                   </div>
                 ))
-              )}
+              }
             </div>
           </CardContent>
         </Card>
